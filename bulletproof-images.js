7a3c6557abaf1bba4058759a8152052aa1185561
filtah <filtah@outlook.com>
2017-08-28 13:57:15 +0200

    add support for contain and images nested in a tags..

--- bulletproof-images.js
+++ bulletproof-images.js
@@ -1,9 +1,37 @@
 //Polyfill
 (function() {
+
+  function hasClass(element, cls) {
+      return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+  }
+
+  function doBackgrounds(bp, child) {
+
+        //hide image..
+        child.style.opacity = 0;
+
+        //copy child url to container background..
+        bp.style.background = "url('" + child.src + "')";
+
+        //set additional style attributes..
+        bp.style.backgroundSize = 'cover';
+        bp.style.backgroundRepeat = 'no-repeat';
+        bp.style.backgroundPosition = 'center center'; 
+        bp.style.backgroundOrigin = 'content-box';
+
+        if ( hasClass(bp, 'bp-contain') ) {
+          bp.style.backgroundSize = 'contain';
+        }
+
+  }
+
+  if ( typeof NodeList.prototype.forEach === "function" ) return false;
+  NodeList.prototype.forEach = Array.prototype.forEach;
+  
   
   if ("objectFit" in document.documentElement.style === false) {
     
-    //console.log("no object fit");
+    console.log("no object fit");
   
     var bps = document.querySelectorAll(".bp");
     if (bps) {
@@ -11,20 +39,26 @@
 
         //only modify if has img tag as immediate child..
         var children = bp.childNodes;
-        children.forEach(function(img) {
-          if (img.tagName === "IMG") {
+        children.forEach(function(ch1) {
+          if (ch1.tagName === "IMG") {
 
-            //hide image..
-            img.style.opacity = 0;
+            doBackgrounds(bp, ch1);
 
-            //copy img url to container background..
-            bp.style.background = "url('" + img.src + "')";
+          }
 
-            //set additional style attributes..
-            bp.style.backgroundSize = 'cover';
-            bp.style.backgroundRepeat = 'no-repeat';
-            bp.style.backgroundPosition = 'center center'; 
-            bp.style.backgroundOrigin = 'content-box';
+          if (ch1.tagName === 'A') {
+
+            var grandChildren = ch1.childNodes;
+            grandChildren.forEach(function(ch2) {
+
+              if (ch2.tagName === "IMG") {
+
+                doBackgrounds(bp, ch2);
+              }
+
+            });
+
+
           }
         });
       });
